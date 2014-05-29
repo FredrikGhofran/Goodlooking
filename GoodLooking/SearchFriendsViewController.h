7790067cd48feab04ca8fc90751ddc891839eaea
FredrikGhofran <fredrik_ghofran@hotmail.com>
2014-05-29 14:19:36 +0200

    added instagram library

---
+++ GoodLooking/SearchFriendsViewController.h
@@ -0,0 +1,14 @@
+//
+//  SearchFriendsViewController.h
+//  GoodLooking
+//
+//  Created by Fredrik Ghofran on 2014-05-29.
+//  Copyright (c) 2014 GoodLooking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchFriendsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
